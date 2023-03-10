b2221da789acc1f5de1d8a6516692ae0c2198624
Ananyobrata Pal <ananyo141@gmail.com>
2023-03-10 22:16:01 +0530

    Add userserializer, userlistview Add login/refresh auth urls remove test_app celery tasks

--- smartshala/core/urls.py
+++ smartshala/core/urls.py
@@ -1,7 +1,18 @@
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from smartshala.core.views import UserListView
 
 urlpatterns = [
     path("grader/", include("smartshala.grader_app.urls")),
     path("test/", include("smartshala.test_app.urls")),
     path("school/", include("smartshala.school_app.urls")),
+    path("user/", UserListView.as_view(), name="user-list"),
+    path(
+        "auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- smartshala/core/views.py
+++ smartshala/core/views.py
@@ -1,3 +1,23 @@
-# from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+
+class UserListView(APIView):
+    """Create a new user in the system"""
+
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = User.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if not user.is_authenticated:
+            return Response({"message": "Please Login"})
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=200)
